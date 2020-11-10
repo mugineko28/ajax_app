a61135304dd67ba64346488f1c370ee227e0ac9a
mugineko28 <foradamstories@yahoo.co.jp>
2020-11-10 21:05:20 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@ ActiveRecord::Schema.define(version: 2020_08_30_104556) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.text "content"
+    t.boolean "checked"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "checked"
   end
 
 end
